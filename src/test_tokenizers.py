4aa88f9bfedc5edac47d948b379d00f265525e61
MeesterDavid <51082774+MeesterDavid@users.noreply.github.com>
2026-07-14 10:28:00 +0200

    p58

--- src/test_tokenizers.py
+++ src/test_tokenizers.py
@@ -1,59 +1,51 @@
 import marimo
 
+__generated_with = "0.23.9"
 app = marimo.App()
 
 
 @app.cell
-def __():
+def _():
     from pathlib import Path
     import re
     from tokenizer import SimpleTokenizerV1
     from data_loader import create_dataloader_v1, tokenizer
     import torch
     from importlib.metadata import version
-    return (
-        Path,
-        SimpleTokenizerV1,
-        create_dataloader_v1,
-        re,
-        tokenizer,
-        torch,
-        version,
-    )
+    import os
+    print(os.getcwd())
+    print("torch version: ", torch.__version__)
+    print(torch.cuda.is_available())
+    return Path, create_dataloader_v1, tokenizer, torch
 
 
 @app.cell
-def __(torch):
-    print("torch version: ", torch.__version__)
-    print(torch.cuda.is_available())
+def _():
     return
 
 
 @app.cell
-def __(Path):
-    verdict_path = Path('data', 'the_verdict.txt')
-    print(f"path is {verdict_path}")
-    return (verdict_path,)
+def _():
+    return
 
 
 @app.cell
-def __(verdict_path):
+def _(Path):
+    notebook_dir = Path(__file__).parent.parent
+    notebook_dir
+    verdict_path = Path(notebook_dir, 'data', 'the_verdict.txt')
+    print(f"path is {verdict_path}")
     with open(verdict_path, "r") as verdict_file:
         verdict_text = verdict_file.read()
     return (verdict_text,)
 
 
 @app.cell
-def __(tokenizer, torch):
+def _(create_dataloader_v1, tokenizer, torch, verdict_text):
     vocab_size = tokenizer.n_vocab
     output_dim = 256
     max_length = 4
     embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-    return embedding_layer, max_length, output_dim, vocab_size
-
-
-@app.cell
-def __(create_dataloader_v1, max_length, verdict_text):
     dataloader = create_dataloader_v1(
         verdict_text,
         batch_size=8,
@@ -61,35 +53,41 @@ def __(create_dataloader_v1, max_length, verdict_text):
         stride=max_length,
         shuffle=False,
     )
-    return (dataloader,)
+    data_iterator = iter(dataloader)
+    inputs, targets = next(data_iterator)
+    token_embeddings = embedding_layer(inputs)
+    return max_length, output_dim, token_embeddings
 
 
 @app.cell
-def __(dataloader):
-    data_iterator = iter(dataloader)
-    inputs, targets = next(data_iterator)
-    print("Token IDs: \n", inputs)
-    print("\nInputs shape:\n", inputs.shape)
-    return data_iterator, inputs, targets
+def _(max_length, output_dim, torch):
+    context_length = max_length
+    positional_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+    positional_embeddings = positional_embedding_layer(torch.arange(context_length))
+    return (positional_embeddings,)
+
+
+@app.cell
+def _(positional_embeddings, token_embeddings):
+    input_embeddings = token_embeddings + positional_embeddings
+    input_embeddings.shape
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
 
 
 if __name__ == "__main__":
     app.run()
-
-# tokenizer = SimpleTokenizerV1(vocab)
-# text1 = "Hello, do you like tea?"
-# text2 = "In the sunlit terraces of the palace."
-# text = " <|endoftext|> ".join((text1, text2))
-# print(text)
-
-# ids = tokenizer.encode(text)
-# print(ids)
-# text = tokenizer.decode(ids)
-# print(text)
-
-# tokenizer = tiktoken.get_encoding("gpt2")
-# text = "Hello, do you like tea? <|endoftext|> In the sunlit terraces of the SUOMENADS."
-# ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-# print(ids)
-# text = tokenizer.decode(ids)
-# print(text)
